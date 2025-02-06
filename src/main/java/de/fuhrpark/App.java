709ff7cfd9672c7b958d600023007563e69d4aed
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-06 03:06:49 +0100

    Add main application class

--- src/main/java/de/fuhrpark/App.java
+++ src/main/java/de/fuhrpark/App.java
@@ -1,5 +1,14 @@
 package de.fuhrpark;
 
+import de.fuhrpark.ui.FuhrparkUI;
+import de.fuhrpark.service.FahrzeugService;
+import de.fuhrpark.service.FahrtenbuchService;
+import de.fuhrpark.service.ReparaturService;
+import de.fuhrpark.service.impl.FahrzeugServiceImpl;
+import de.fuhrpark.service.impl.FahrtenbuchServiceImpl;
+import de.fuhrpark.service.impl.ReparaturServiceImpl;
+import javax.swing.SwingUtilities;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SwingUtilities.invokeLater(() -> {
+            FahrzeugService fahrzeugService = new FahrzeugServiceImpl();
+            FahrtenbuchService fahrtenbuchService = new FahrtenbuchServiceImpl();
+            ReparaturService reparaturService = new ReparaturServiceImpl();
+            
+            FuhrparkUI ui = new FuhrparkUI(fahrzeugService, fahrtenbuchService, reparaturService);
+            ui.setVisible(true);
+        });
     }
 }
