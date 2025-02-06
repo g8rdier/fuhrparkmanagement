c4cc7e70c7a94e90ada6fadc96019e3a578d2131
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-06 04:43:23 +0100

    feat: add service interfaces for Fahrtenbuch and Reparatur

--- src/main/java/de/fuhrpark/service/FahrtenbuchService.java
+++ src/main/java/de/fuhrpark/service/FahrtenbuchService.java
@@ -4,6 +4,6 @@ import de.fuhrpark.model.FahrtenbuchEintrag;
 import java.util.List;
 
 public interface FahrtenbuchService {
-    void addEintrag(FahrtenbuchEintrag eintrag);
+    void addFahrtenbuchEintrag(FahrtenbuchEintrag eintrag);
     List<FahrtenbuchEintrag> getEintraegeForFahrzeug(String kennzeichen);
 } 
