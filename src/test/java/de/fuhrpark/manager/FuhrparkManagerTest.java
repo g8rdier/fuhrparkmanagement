a27ae697dbc0f64b41f33704be4c06e3d345e1b9
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-06 04:19:09 +0100

    feat: add basic functionality test for FuhrparkManager

--- src/test/java/de/fuhrpark/manager/FuhrparkManagerTest.java
+++ src/test/java/de/fuhrpark/manager/FuhrparkManagerTest.java
@@ -1,5 +1,10 @@
 package de.fuhrpark.manager;
 
+import de.fuhrpark.model.Fahrzeug;
+import de.fuhrpark.model.FahrtenbuchEintrag;
+import de.fuhrpark.model.ReparaturBuchEintrag;
+import de.fuhrpark.model.enums.FahrzeugTyp;
+import de.fuhrpark.model.enums.ReparaturTyp;
 import de.fuhrpark.persistence.DataStore;
 import de.fuhrpark.persistence.impl.DataStoreImpl;
 import de.fuhrpark.service.impl.FahrzeugServiceImpl;
@@ -7,6 +12,9 @@ import de.fuhrpark.service.impl.FahrtenbuchServiceImpl;
 import de.fuhrpark.service.impl.ReparaturServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
 
 public class FuhrparkManagerTest {
     private DataStore dataStore;
@@ -23,7 +31,42 @@ public class FuhrparkManagerTest {
     }
 
     @Test
-    void testBasicFunctionality() {
-        // Test implementation will come later
+    void testFahrzeugManagement() {
+        Fahrzeug testFahrzeug = new Fahrzeug(
+            "B-TK-2023",
+            "Tesla",
+            "Model 3",
+            FahrzeugTyp.PKW,
+            2023,
+            45000.0
+        );
+
+        fahrzeugService.addFahrzeug(testFahrzeug);
+        assertEquals(1, fahrzeugService.getAlleFahrzeuge().size());
+        
+        FahrtenbuchEintrag fahrtenbuchEintrag = new FahrtenbuchEintrag(
+            LocalDate.now(),
+            "Berlin",
+            "München",
+            500.0,
+            testFahrzeug.getKennzeichen()
+        );
+        fahrtenbuchService.addEintrag(fahrtenbuchEintrag);
+
+        ReparaturBuchEintrag reparaturEintrag = new ReparaturBuchEintrag(
+            LocalDate.now(),
+            ReparaturTyp.INSPEKTION,
+            "Jahresinspektion",
+            500.0,
+            testFahrzeug.getKennzeichen(),
+            "Werkstatt Berlin"
+        );
+        reparaturService.addReparatur(reparaturEintrag);
+
+        var fahrten = fahrtenbuchService.getEintraegeForFahrzeug(testFahrzeug.getKennzeichen());
+        assertEquals(1, fahrten.size());
+
+        var reparaturen = reparaturService.getReparaturenForFahrzeug(testFahrzeug.getKennzeichen());
+        assertEquals(1, reparaturen.size());
     }
 } 
