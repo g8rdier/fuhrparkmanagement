f7b24fe004665eaf48927cc7d8ad758c23747f4c
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-07 02:36:30 +0100

    fix: move and update FuhrparkManager and test to correct package

--- src/main/java/de/fuhrpark/manager/FuhrparkManager.java
+++ src/main/java/de/fuhrpark/manager/FuhrparkManager.java
@@ -1,13 +1,11 @@
 // src/main/java/de/fuhrpark/manager/FuhrparkManager.java
 package de.fuhrpark.manager;
 
-import de.fuhrpark.model.*;
-import de.fuhrpark.persistence.DataStore;
-import de.fuhrpark.service.*;
-import de.fuhrpark.service.impl.*;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import de.fuhrpark.model.Fahrzeug;
+import de.fuhrpark.model.ReparaturBuchEintrag;
+import de.fuhrpark.service.FahrzeugService;
+import de.fuhrpark.service.FahrtenbuchService;
+import de.fuhrpark.service.ReparaturService;
 
 /**
  * Verwaltet die Fahrzeuge des Fuhrparks.
@@ -17,50 +15,27 @@ public class FuhrparkManager {
     private final FahrtenbuchService fahrtenbuchService;
     private final ReparaturService reparaturService;
 
-    public FuhrparkManager(DataStore dataStore) {
-        this.fahrzeugService = new FahrzeugServiceImpl(dataStore);
-        this.fahrtenbuchService = new FahrtenbuchServiceImpl(dataStore);
-        this.reparaturService = new ReparaturServiceImpl(dataStore);
+    public FuhrparkManager(FahrzeugService fahrzeugService, 
+                          FahrtenbuchService fahrtenbuchService,
+                          ReparaturService reparaturService) {
+        this.fahrzeugService = fahrzeugService;
+        this.fahrtenbuchService = fahrtenbuchService;
+        this.reparaturService = reparaturService;
     }
 
-    // Fahrzeug management
     public void addFahrzeug(Fahrzeug fahrzeug) {
-        fahrzeugService.addFahrzeug(fahrzeug);
+        fahrzeugService.saveFahrzeug(fahrzeug);
+    }
+
+    public void addReparatur(String kennzeichen, ReparaturBuchEintrag reparatur) {
+        reparaturService.addReparatur(kennzeichen, reparatur);
+    }
+
+    public Fahrzeug getFahrzeug(String kennzeichen) {
+        return fahrzeugService.getFahrzeugByKennzeichen(kennzeichen);
     }
 
     public void deleteFahrzeug(String kennzeichen) {
         fahrzeugService.deleteFahrzeug(kennzeichen);
-    }
-
-    public List<Fahrzeug> getAlleFahrzeuge() {
-        return fahrzeugService.getAlleFahrzeuge();
-    }
-
-    public Fahrzeug getFahrzeugByKennzeichen(String kennzeichen) {
-        return fahrzeugService.getFahrzeugByKennzeichen(kennzeichen);
-    }
-
-    public List<Fahrzeug> filterFahrzeuge(Predicate<Fahrzeug> filter) {
-        return fahrzeugService.getAlleFahrzeuge().stream()
-                .filter(filter)
-                .collect(Collectors.toList());
-    }
-
-    // Fahrtenbuch management
-    public void addEintrag(FahrtenbuchEintrag eintrag) {
-        fahrtenbuchService.addEintrag(eintrag);
-    }
-
-    public List<FahrtenbuchEintrag> getFahrtenbuchEintraege(String kennzeichen) {
-        return fahrtenbuchService.getEintraegeForFahrzeug(kennzeichen);
-    }
-
-    // Reparatur management
-    public void addReparatur(ReparaturBuchEintrag reparatur) {
-        reparaturService.addReparatur(reparatur);
-    }
-
-    public List<ReparaturBuchEintrag> getReparaturen(String kennzeichen) {
-        return reparaturService.getReparaturenForFahrzeug(kennzeichen);
     }
 }
--- src/test/java/de/fuhrpark/manager/FuhrparkManagerTest.java
+++ src/test/java/de/fuhrpark/manager/FuhrparkManagerTest.java
@@ -1,19 +1,15 @@
 package de.fuhrpark.manager;
 
 import de.fuhrpark.model.Fahrzeug;
-import de.fuhrpark.model.FahrtenbuchEintrag;
 import de.fuhrpark.model.ReparaturBuchEintrag;
 import de.fuhrpark.model.enums.FahrzeugTyp;
-import de.fuhrpark.model.enums.ReparaturTyp;
-import de.fuhrpark.persistence.DataStore;
-import de.fuhrpark.persistence.impl.DataStoreImpl;
 import de.fuhrpark.service.FahrzeugService;
 import de.fuhrpark.service.FahrtenbuchService;
 import de.fuhrpark.service.ReparaturService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
@@ -102,59 +98,4 @@ public class FuhrparkManagerTest {
         // Then
         verify(fahrzeugService).deleteFahrzeug(kennzeichen);
     }
-
-    @Test
-    void testFahrzeugManagement() {
-        Fahrzeug testFahrzeug = new Fahrzeug(
-            "B-TK-2023",
-            "Tesla",
-            "Model 3",
-            FahrzeugTyp.PKW,
-            2023,
-            45000.0
-        );
-
-        fahrzeugService.addFahrzeug(testFahrzeug);
-        assertEquals(1, fahrzeugService.getAlleFahrzeuge().size());
-        
-        FahrtenbuchEintrag fahrtenbuchEintrag = new FahrtenbuchEintrag(
-            LocalDate.now(),
-            "Berlin",
-            "München",
-            500.0,
-            testFahrzeug.getKennzeichen()
-        );
-        fahrtenbuchService.addEintrag(fahrtenbuchEintrag);
-
-        ReparaturBuchEintrag reparaturEintrag = new ReparaturBuchEintrag(
-            LocalDate.now(),
-            ReparaturTyp.INSPEKTION,
-            "Jahresinspektion",
-            500.0,
-            testFahrzeug.getKennzeichen(),
-            "Werkstatt Berlin"
-        );
-        reparaturService.addReparatur(reparaturEintrag);
-
-        var fahrten = fahrtenbuchService.getEintraegeForFahrzeug(testFahrzeug.getKennzeichen());
-        assertEquals(1, fahrten.size());
-
-        var reparaturen = reparaturService.getReparaturenForFahrzeug(testFahrzeug.getKennzeichen());
-        assertEquals(1, reparaturen.size());
-    }
-
-    @Test
-    void testAddFahrtenbuchEintrag() {
-        FahrtenbuchEintrag eintrag = new FahrtenbuchEintrag(
-            LocalDate.now(),
-            "Berlin",
-            "Hamburg",
-            500.0,
-            "B-AA 1234",
-            "Max Mustermann"
-        );
-        fahrtenbuchService.addEintrag(eintrag);
-        var eintraege = fahrtenbuchService.getEintraegeForFahrzeug("B-AA 1234");
-        assertTrue(eintraege.contains(eintrag));
-    }
-} 
+}
