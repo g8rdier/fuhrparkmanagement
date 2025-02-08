14b2bf470ab0eabda225dee56291dd02c9dbf231
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-08 20:50:33 +0100

    refactor: remove unused dataStore variable

--- src/main/java/de/fuhrpark/App.java
+++ src/main/java/de/fuhrpark/App.java
@@ -1,19 +1,9 @@
 package de.fuhrpark;
 
-import de.fuhrpark.persistence.DataStore;
-import de.fuhrpark.persistence.impl.DataStoreImpl;
-import de.fuhrpark.service.FahrzeugService;
-import de.fuhrpark.service.FahrtenbuchService;
-import de.fuhrpark.service.impl.FahrzeugServiceImpl;
-import de.fuhrpark.service.impl.FahrtenbuchServiceImpl;
 import de.fuhrpark.ui.FuhrparkUI;
 
 public class App {
     public static void main(String[] args) {
-        DataStore dataStore = new DataStoreImpl();
-        FahrzeugService fahrzeugService = new FahrzeugServiceImpl(dataStore);
-        FahrtenbuchService fahrtenbuchService = new FahrtenbuchServiceImpl(dataStore);
-        
         javax.swing.SwingUtilities.invokeLater(() -> {
             new FuhrparkUI();
         });
