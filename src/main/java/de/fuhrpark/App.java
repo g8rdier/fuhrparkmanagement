cb0b1bed200ac2807cade24325f2318ec011a23a
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-08 20:51:40 +0100

    fix: make UI visible when launching app

--- src/main/java/de/fuhrpark/App.java
+++ src/main/java/de/fuhrpark/App.java
@@ -5,7 +5,8 @@ import de.fuhrpark.ui.FuhrparkUI;
 public class App {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
-            new FuhrparkUI();
+            FuhrparkUI ui = new FuhrparkUI();
+            ui.setVisible(true);
         });
     }
 }
