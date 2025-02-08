746848c20c32f769f7302f0bd59e55eb425d8b33
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-08 19:21:03 +0100

    Simplify DataStore interface to only handle Fahrzeug operations

--- src/main/java/de/fuhrpark/persistence/DataStore.java
+++ src/main/java/de/fuhrpark/persistence/DataStore.java
@@ -1,33 +1,11 @@
 package de.fuhrpark.persistence;
 
 import de.fuhrpark.model.Fahrzeug;
-import de.fuhrpark.model.FahrtenbuchEintrag;
-import de.fuhrpark.model.ReparaturBuchEintrag;
 import java.util.List;
 
 public interface DataStore {
-    void addFahrzeug(Fahrzeug fahrzeug);
-    void updateFahrzeug(Fahrzeug fahrzeug);
     void saveFahrzeug(Fahrzeug fahrzeug);
-    void deleteFahrzeug(String kennzeichen);
     Fahrzeug getFahrzeug(String kennzeichen);
-    List<Fahrzeug> getFahrzeuge();
     List<Fahrzeug> getAlleFahrzeuge();
-    Fahrzeug getFahrzeugByKennzeichen(String kennzeichen);
-    
-    void addFahrtenbuchEintrag(FahrtenbuchEintrag eintrag);
-    List<FahrtenbuchEintrag> getFahrtenbuchEintraege();
-    List<FahrtenbuchEintrag> getFahrtenbuchEintraege(String kennzeichen);
-    
-    void addReparaturBuchEintrag(ReparaturBuchEintrag eintrag);
-    void saveReparatur(String kennzeichen, ReparaturBuchEintrag eintrag);
-    List<ReparaturBuchEintrag> getReparaturBuchEintraege();
-    List<ReparaturBuchEintrag> getReparaturen();
-    List<ReparaturBuchEintrag> getReparaturen(String kennzeichen);
-    
-    void save(String filename, Object data);
-    Object load(String filename);
-
-    void saveFahrt(String kennzeichen, FahrtenbuchEintrag fahrt);
-    List<FahrtenbuchEintrag> getFahrten(String kennzeichen);
+    void deleteFahrzeug(String kennzeichen);
 }
