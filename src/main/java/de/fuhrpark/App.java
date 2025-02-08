50120639beee1c8dac3ecac42719a0a1e20a8278
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-08 20:00:15 +0100

    refactor: simplify App.java temporarily while UI class is determined

--- src/main/java/de/fuhrpark/App.java
+++ src/main/java/de/fuhrpark/App.java
@@ -1,28 +1,13 @@
 package de.fuhrpark;
 
-import de.fuhrpark.manager.FuhrparkManager;
 import de.fuhrpark.persistence.DataStore;
 import de.fuhrpark.persistence.impl.DataStoreImpl;
-import de.fuhrpark.ui.FuhrparkUI;
 
 public class App {
-    private final FuhrparkManager manager;
-
-    public App() {
-        DataStore dataStore = new DataStoreImpl();
-        this.manager = new FuhrparkManager(dataStore);
-    }
-
-    public void start() {
-        // Create and show the main UI
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            FuhrparkUI ui = new FuhrparkUI();
-            ui.setVisible(true);
-        });
-    }
-
     public static void main(String[] args) {
-        App app = new App();
-        app.start();
+        DataStore dataStore = new DataStoreImpl();
+        
+        // TODO: Add correct UI initialization once we know the proper class
+        System.out.println("Services initialized successfully");
     }
 }
--- src/main/java/de/fuhrpark/model/PKW.java
+++ src/main/java/de/fuhrpark/model/PKW.java
@@ -3,10 +3,11 @@ package de.fuhrpark.model;
 import de.fuhrpark.model.enums.FahrzeugTyp;
 
 public class PKW extends Fahrzeug {
-    public PKW(String kennzeichen) {
-        super(kennzeichen);
-    }
     
+    public PKW(String kennzeichen) {
+        this.kennzeichen = kennzeichen;
+    }
+
     @Override
     public FahrzeugTyp getTyp() {
         return FahrzeugTyp.PKW;
@@ -14,6 +15,6 @@ public class PKW extends Fahrzeug {
 
     @Override
     public String toString() {
-        return "PKW [Kennzeichen=" + getKennzeichen() + "]";
+        return "PKW [Kennzeichen=" + kennzeichen + "]";
     }
 } 
