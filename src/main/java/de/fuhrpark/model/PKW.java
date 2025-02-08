afe0ba23ed2ec350603a00a47656a47330b38b74
Kobilarov, Gregor <g.kobilarov@purpose.hr>
2025-02-08 19:43:36 +0100

    fix: update PKW class to properly extend Fahrzeug

--- src/main/java/de/fuhrpark/model/PKW.java
+++ src/main/java/de/fuhrpark/model/PKW.java
@@ -1,12 +1,19 @@
 package de.fuhrpark.model;
 
+import de.fuhrpark.model.enums.FahrzeugTyp;
+
 public class PKW extends Fahrzeug {
     public PKW(String kennzeichen) {
         super(kennzeichen);
     }
     
     @Override
-    public String getTyp() {
-        return "PKW";
+    public FahrzeugTyp getTyp() {
+        return FahrzeugTyp.PKW;
+    }
+
+    @Override
+    public String toString() {
+        return "PKW [Kennzeichen=" + getKennzeichen() + "]";
     }
 } 
--- src/main/java/de/fuhrpark/persistence/DataStore.java
+++ src/main/java/de/fuhrpark/persistence/DataStore.java
@@ -1,11 +1,17 @@
 package de.fuhrpark.persistence;
 
 import de.fuhrpark.model.Fahrzeug;
+import de.fuhrpark.model.FahrtenbuchEintrag;
 import java.util.List;
 
 public interface DataStore {
+    // Fahrzeug methods
     void saveFahrzeug(Fahrzeug fahrzeug);
     Fahrzeug getFahrzeug(String kennzeichen);
     List<Fahrzeug> getAlleFahrzeuge();
     void deleteFahrzeug(String kennzeichen);
+    
+    // Fahrtenbuch methods
+    void saveFahrt(String kennzeichen, FahrtenbuchEintrag eintrag);
+    List<FahrtenbuchEintrag> getFahrten(String kennzeichen);
 }
